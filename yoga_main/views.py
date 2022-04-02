33b7da3bfb0f78b45822d83e5944921d21ba3058
sathvikbhavaraju <sathvikb21@gmail.com>
2022-04-02 13:07:48 -0400

    added signup

--- yoga_main/views.py
+++ yoga_main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def home(request):
@@ -10,3 +10,8 @@ def superpanel(request):
 
 def login(request):
   return render(request, 'login.html')
+
+def signup(request):
+  form = UserCreationForm()
+  context = {'form': form}
+  return render(request, 'signup.html', context)
