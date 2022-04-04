345f39c279a9797ddaa5bd0cfe8a03078dea824a
jahantaila <tailajahan@gmail.com>
2022-04-04 19:00:14 -0400

    fixed login required

--- yoga_main/views.py
+++ yoga_main/views.py
@@ -2,11 +2,13 @@ from cmath import log
 from pyexpat.errors import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def home(request):
   return render(request, 'home.html')
 
+@login_required(login_url = ('/home/')) #
 def superpanel(request):
   return render(request, 'teachpanel.html')
 
