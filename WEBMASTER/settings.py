da59c8085ec778c905d9eb133a9e33cf69bfd78f
Prasit Dhungyel <=>
2022-04-18 01:25:02 -0400

    commit

--- WEBMASTER/settings.py
+++ WEBMASTER/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import django_heroku
+django_heroku.settings(locals())
 import os
 from pathlib import Path
 
