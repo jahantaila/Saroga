c0a26b7d484f3eeca3813313e8a870bbaa542c77
Prasit Dhungyel <=>
2022-04-18 12:37:43 -0400

    SATHVIK IS THE BEST. HE IS DADDY

--- WEBMASTER/settings.py
+++ WEBMASTER/settings.py
@@ -9,12 +9,9 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import django_heroku
-django_heroku.settings(locals())
 import os
 from pathlib import Path
-import django_heroku
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
