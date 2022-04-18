d6d4fb335de0183c4e6911d6e32417a6ab601910
jahantaila <tailajahan@gmail.com>
2022-04-17 22:53:12 -0400

    working on about screen

--- yoga_main/views.py
+++ yoga_main/views.py
@@ -60,7 +60,7 @@ def loginUser(request):
 			password =request.POST.get('password')
 
 			user = authenticate(username=username, password=password)
-
+      
 			if user is not None:
 				login(request, user)
 				return redirect('/dashboard')
